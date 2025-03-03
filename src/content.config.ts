1310fa257f5863823aa4c04d96bc4e65dab324fe
Shams Hasan <shamshasan941@gmail.com>
2025-03-02 23:22:11 -0500

    1) centered nav bar headings for desktop & mobile 2) added routing to reading list link 3) added in book review

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,32 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
+	type: 'content',
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		math: z.boolean().optional(),
 	}),
 });
 
-export const collections = { blog };
+const bookSchema = z.object({
+    title: z.string(),
+    readDate: z.object({
+        start: z.coerce.string().nullable(),
+        end: z.coerce.string().nullable(),
+    }),
+    author: z.string(),
+    review: z.string(), // Add this line
+    rating: z.number().min(0).max(5).nullable(),
+});
+
+const book = defineCollection({
+	type: 'content',
+	schema: bookSchema,
+});
+
+export type Book = z.infer<typeof bookSchema>;
+
+export const collections = { blog, book };
---
+++ src/content/book/grokking-algorithms.md
@@ -0,0 +1,15 @@
+---
+title: 'Grokking Algorithms — 2nd edition'
+review: >
+  This book offers a deep dive into essential data structures (e.g., hashmaps, queues, binary trees) 
+  and algorithms (e.g., quick sort, selection sort, binary search). While I felt the binary search 
+  chapter was a bit premature without foundational concepts like linear search, I found the book 
+  excels with its clear, practical code examples. Overall, I think it's a highly informative and 
+  well-structured resource.
+pubDate: 'Feb 24 2025'
+readDate:
+  start: '2024-09-10'
+  end: 'Present'
+author: 'Aditya Y. Bhargava'
+rating: 5
+---
